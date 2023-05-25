225f65f7b1600362d50d5f183aa39f64da9f7242
bastndev <gohit24@gmail.com>
2023-05-25 17:33:50 -0500

    :arrow_up: Quiz `v6.0.4`

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+  const ResultsScreen({
+    super.key,
+    required this.chosenAnswers,
+  });
+
+  final List<String> chosenAnswers;
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +16,13 @@ class ResultsScreen extends StatelessWidget {
         padding: const EdgeInsets.all(50),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children:  [
+          children: [
             const Text('CAlla hello world 2023 esa  alamos'),
             const SizedBox(height: 30),
             const Text('List of answers and question'),
             const SizedBox(height: 30),
             TextButton(
-              onPressed: (){},
+              onPressed: () {},
               child: const Text('Restart Quiz'),
             )
           ],
