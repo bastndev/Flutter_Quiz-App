98779f87336c936ab687cddac29bb2be259c6d13
bastndev <gohit24@gmail.com>
2023-05-26 10:20:27 -0500

    :arrow_up: new Quiz

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
+import 'package:adv_basics/data/question.dart';
+
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({
-    super.key,
-    required this.chosenAnswers,
-  });
+  const ResultsScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i]
+        },
+      );
+    }
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
